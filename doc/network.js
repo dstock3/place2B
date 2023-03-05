9bea8c02fe5905114dadbc789bbb7036f5907eff
dstock3 <dstock3@gmail.com>
2023-03-04 19:03:47 -0600

    revised layout

--- doc/network.js
+++ doc/network.js
@@ -4,23 +4,22 @@ const addNetworkInterfaces = getNetworkInterfaces => {
     const networksContainer = document.getElementById('network-interface-container');
 
     async function displayNetworkInterfaces() {
-    const networkInterfaces = await getNetworkInterfaces();
-    console.log(networkInterfaces)
+        const networkInterfaces = await getNetworkInterfaces();
+        console.log(networkInterfaces)
 
-    networkInterfaces.forEach((iface) => {
-        const ifaceDiv = document.createElement('div');
-        const nameSpan = document.createElement('span');
-        const addressSpan = document.createElement('span');
-        nameSpan.textContent = `Name: ${iface.name}`;
-        addressSpan.textContent = `Address: ${iface.address}`;
-        ifaceDiv.appendChild(nameSpan);
-        ifaceDiv.appendChild(addressSpan);
-        networksContainer.appendChild(ifaceDiv);
-    });
+        networkInterfaces.forEach((iface) => {
+            const ifaceDiv = document.createElement('div');
+            const nameSpan = document.createElement('span');
+            const addressSpan = document.createElement('span');
+            nameSpan.textContent = `Name: ${iface.name}`;
+            addressSpan.textContent = `Address: ${iface.address}`;
+            ifaceDiv.appendChild(nameSpan);
+            ifaceDiv.appendChild(addressSpan);
+            networksContainer.appendChild(ifaceDiv);
+        });
     }
 
     displayNetworkInterfaces();
 }
-
 
 module.exports = { addNetworkInterfaces }
