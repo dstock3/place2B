fca006ccaf4ea3a4333d6ae72aa61c4457eb8295
dstock3 <dstock3@gmail.com>
2023-03-05 12:35:21 -0600

    employed data attribute in tabSelect module

--- doc/tabSelect.js
+++ doc/tabSelect.js
@@ -4,18 +4,17 @@ const tabSelect = () => {
     tabs.forEach((tab) => {
         tab.addEventListener('click', function() {
             tab.classList.add('selected');
-            let tabId = tab.id
+            
             for (let i = 0; i < tabs.length; i++) {
                 if (tab !== tabs[i]) {
                     tabs[i].classList.remove('selected');
                 }
             }
             
-            let panelControllerId = tabId.substring(tabId.length - 1);
             const displays = Array.from(document.getElementsByClassName("panel-container"))
             
             for (let x = 0; x < displays.length; x++) {
-                if (panelControllerId === displays[x].id) {
+                if (tab.dataset.panel === displays[x].id) {
                     displays[x].classList.add("active");
                     displays[x].classList.remove("hidden");
                 } else {
