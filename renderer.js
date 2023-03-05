356935e44ca102e6e6ab02a92d69ae68e5983063
dstock3 <dstock3@gmail.com>
2023-03-04 21:03:21 -0600

    Added functionality for tab select.

--- renderer.js
+++ renderer.js
@@ -3,6 +3,36 @@ const { addMacChanger } = require('./doc/macchanger')
 const { addNetworkInterfaces } = require('./doc/network')
 const { getNetworkInterfaces } = require('./tools/network');
 const { renderTerminal } = require('./doc/terminal')
+
+/* tab select */
+
+const tabs = Array.from(document.getElementsByClassName('tab'));
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', function() {
+    tab.classList.add('selected');
+    let tabId = tab.id
+    for (let i = 0; i < tabs.length; i++) {
+      if (tab !== tabs[i]) {
+        tabs[i].classList.remove('selected');
+      }
+    }
+    
+    let panelControllerId = tabId.substring(tabId.length - 1);
+    const displays = Array.from(document.getElementsByClassName("panel-container"))
+    for (let x = 0; x < displays.length; x++) {
+      if (panelControllerId === displays[x].id) {
+        displays[x].classList.add("active");
+        displays[x].classList.remove("hidden");
+      } else {
+        displays[x].classList.add("hidden");
+        displays[x].classList.remove("active");
+
+      }
+    }
+  });
+});
+
 /*
 try {
   const MyTerminal = require('./tools/terminal')
